c6eb383727317b7b7250ef0279b645c4cc48c04d
boscha1 <bosch.anthony7@gmail.com>
2021-09-22 11:47:18 -0400

    Added test case for insert_tail()

--- LinkedListTest.py
+++ LinkedListTest.py
@@ -6,6 +6,7 @@ class MyTestCase(unittest.TestCase):
     """
         Test cases for LinkedList class
     """
+
     def setUp(self):
         self.linked_list = LinkedList()
 
@@ -21,7 +22,6 @@ class MyTestCase(unittest.TestCase):
         """
         self.assertEqual(self.linked_list.to_string(), " Tim -> Fred -> John -> None")
 
-
     def test_insert_head(self):
         """
             Test the insert_head() method to see if a new node is inserted at
@@ -29,6 +29,14 @@ class MyTestCase(unittest.TestCase):
         """
         self.linked_list.insert_head("Joe")
         self.assertEqual(self.linked_list.to_string(), " Joe -> Tim -> Fred -> John -> None")
+
+    def test_insert_tail(self):
+        """
+            Test the insert_tail() method to see if a new node is inserted at
+            the end of the LinkedList
+        """
+        self.linked_list.insert_tail("Joe")
+        self.assertEqual(self.linked_list.to_string(), " Tim -> Fred -> John -> Joe -> None")
 
 
 if __name__ == '__main__':
