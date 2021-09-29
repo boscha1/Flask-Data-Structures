386e81fb113c5a3a5f800e8ac776e177807e321c
boscha1 <bosch.anthony7@gmail.com>
2021-09-28 20:24:21 -0400

    Added documentation

--- DataStructures/Stack/StackTest.py
+++ DataStructures/Stack/StackTest.py
@@ -16,15 +16,21 @@ class MyTestCase(unittest.TestCase):
     def test_peek(self):
         """
             Test the peek() method to see if it returns correctly
-        :return: first Node object in the stack
         """
         self.assertEqual(self.stack.peek(), "Tim")  # add assertion here
 
     def test_push(self):
+        """
+            Test the push() method to see if a new node is inserted at the beginning
+            of the stack
+        """
         self.stack.push("Joe")
         self.assertEqual(self.stack.peek(), "Joe")
 
     def test_pop(self):
+        """
+            Test the pop() method to see if the first node in the stack is deleted
+        """
         self.stack.pop()
         self.assertEqual(self.stack.peek(), "Fred")
 
